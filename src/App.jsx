fb3597563b2572758382c8e514191418d284a2b0
Goutam Adhikari Dipu <gtm10322@gmail.com>
2025-04-17 11:07:50 +0600

    my boipoka all install done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+            
+       <h1 className='text-center font-semibold '>hello world</h1>
+       <button className="btn btn-primary">Primary</button>
+       <h1 className=' text-blue-700 '>hello </h1>
+
+    </>
+  )
+}
+
+export default App
